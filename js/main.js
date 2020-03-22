91abbe2780419a413cdeed2d8290769d2297a572
Steven Coaila Zaa <scoailazaa@gmail.com>
2020-03-22 18:39:29 -0500

    Extrayendo datos del api y mostrandolos en el container

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
-function getUsuario()
-{
-  return fetch('https://swapi.co/api/people')
+function init() {
+  fetch('https://swapi.co/api/people/')
+  .then(function(response) {
+    return response.json();
+  }).then(function(users){
+    users.results.forEach(user => {
+      insertCard(createCard(user))
+    })
+  })
 }
 
-getUsuario()
-  .then(data => data.json())
-  .then(users => {
+function createCard(user) {
+  const { 
+    name,
+    height,
+    mass,
+    hair_color,
+    skin_color,
+    eye_color,
+    birth_year,
+    gender,
+  } = user;
 
-  })
+  const card = document.createElement('article');
+  card.innerHTML = `
+    <h1>${name}</h1>
+    <p>${height}</p>
+    <p>${mass}</p>
+    <p>${hair_color}</p>
+    <p>${skin_color}</p>
+    <p>${eye_color}</p>
+    <p>${birth_year}</p>
+    <p>${gender}</p>
+  `;
+
+  return card;
+}
+
+function insertCard(card) {
+  let container = document.getElementById('js_container');
+  container.appendChild(card)
+}
+
+init()
